cee22b13a7b9b68343c4364e8d607a38942fcffc
Guilherme Mateus Sousa Santos <domiuaunoob@outlook.com>
2022-07-31 16:44:59 -0300

    Começando aplicativo de conversões 2 ferramentas, calculadora, front adicionado e teclado universal programado

---
+++ app/src/main/java/com/example/myapplicationz/testegit.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testegit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testegit);
+    }
+}
